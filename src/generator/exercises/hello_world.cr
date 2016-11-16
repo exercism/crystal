0a921c866f208699e2c77dca5f8bdf5e47344d9d
Bryan Mulvihill <mulvihill@informz.com>
2016-11-16 12:49:09 -0500

    Exercise generator

---
+++ src/generator/exercises/hello_world.cr
@@ -0,0 +1,35 @@
+require "./exercise_generator"
+require "./exercise_test_case"
+
+class HelloWorldGenerator < ExerciseGenerator
+
+  def exercise_name
+    "hello-world"
+  end
+
+  def test_cases
+    JSON.parse(data)["cases"].map do |test_case|
+      HelloWorldTestCase.from_json(test_case.to_json)
+    end
+  end
+end
+
+class HelloWorldTestCase < ExerciseTestCase
+  JSON.mapping(
+    description: String,
+    name: String | Nil,
+    expected: String
+  )
+
+  def workload
+    if !name
+      "HelloWorld.hello.should eq(\"Hello, World\")"
+    else
+      "HelloWorld.hello(\"#{name}\").should eq(\"Hello, #{name}\")"
+    end
+  end
+
+  def test_name
+    description
+  end
+end

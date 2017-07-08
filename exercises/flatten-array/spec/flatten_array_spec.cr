a0534a34c7b7c6ffcb1d5217e370190978e105ee
Max Helmetag <max.helmetag@procore.com>
2017-07-08 12:55:09 -0700

    update flatten array spec

--- exercises/flatten-array/spec/flatten_array_spec.cr
+++ exercises/flatten-array/spec/flatten_array_spec.cr
@@ -2,23 +2,27 @@ require "spec"
 require "../src/*"
 
 describe "FlattenArray" do
-  it "tests flattens array with just integers present" do
+  it "no nesting" do
+    FlattenArray.flatten([0, 1, 2]).should eq([0, 1, 2])
+  end
+
+  pending "flattens array with just integers present" do
     FlattenArray.flatten([1, [2, 3, 4, 5, 6, 7], 8]).should eq([1, 2, 3, 4, 5, 6, 7, 8])
   end
 
-  pending "tests 5 level nesting" do
+  pending "5 level nesting" do
     FlattenArray.flatten([0, 2, [[2, 3], 8, 100, 4, [[[50]]]], -2]).should eq([0, 2, 2, 3, 8, 100, 4, 50, -2])
   end
 
-  pending "tests 6 level nesting" do
+  pending "6 level nesting" do
     FlattenArray.flatten([1, [2, [[3]], [4, [[5]]], 6, 7], 8]).should eq([1, 2, 3, 4, 5, 6, 7, 8])
   end
 
-  pending "tests 6 level nest list with null values" do
+  pending "6 level nest list with null values" do
     FlattenArray.flatten([0, 2, [[2, 3], 8, [[100]], nil, [[nil]]], -2]).should eq([0, 2, 2, 3, 8, 100, -2])
   end
 
-  pending "tests all values in nested list are null" do
+  pending "all values in nested list are null" do
     FlattenArray.flatten([nil, [[[nil]]], nil, nil, [[nil, nil], nil], nil]).should eq([] of Nil)
   end
 end

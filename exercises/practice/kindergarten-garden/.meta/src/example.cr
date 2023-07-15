06f4483e6d14ac329804cc2443ed3ecf3597655b
Ryan Hartlage <ryanplusplus@gmail.com>
2023-07-15 09:09:42 -0400

    Example solution

--- exercises/practice/kindergarten-garden/.meta/src/example.cr
+++ exercises/practice/kindergarten-garden/.meta/src/example.cr
@@ -1,3 +1,37 @@
-module KindergartenGarden
-  # Write your code for the 'Kindergarten Garden' exercise in this file.
+class KindergartenGarden
+  def initialize(@diagram : String)
+  end
+
+  def plants(student)
+    i = students.index!(student)
+    @diagram.split("\n").map do |row|
+      row[(2 * i)..(2 * i + 1)]
+    end.join.chars.map { |char| plant(char) }
+  end
+
+  private def students
+    [
+      "Alice",
+      "Bob",
+      "Charlie",
+      "David",
+      "Eve",
+      "Fred",
+      "Ginny",
+      "Harriet",
+      "Ileana",
+      "Joseph",
+      "Kincaid",
+      "Larry",
+    ]
+  end
+
+  private def plant(char)
+    {
+      'C' => "clover",
+      'G' => "grass",
+      'R' => "radishes",
+      'V' => "violets",
+    }[char]
+  end
 end
--- exercises/practice/kindergarten-garden/src/kindergarten_garden.cr
+++ exercises/practice/kindergarten-garden/src/kindergarten_garden.cr
@@ -1,3 +1,3 @@
-module KindergartenGarden
+class KindergartenGarden
   # Write your code for the 'Kindergarten Garden' exercise in this file.
 end

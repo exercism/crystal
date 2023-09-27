b8064c8aa38ff4bd243cd40b10399b930bcb6299
Kah Goh <villastar@yahoo.com.au>
2023-09-27 21:39:10 +0800

    Create template for simple-cipher tests

--- exercises/practice/simple-cipher/spec/simple_cipher_spec.cr
+++ exercises/practice/simple-cipher/spec/simple_cipher_spec.cr
@@ -3,64 +3,64 @@ require "../src/*"
 
 describe "SimpleCipher" do
   describe "Random key cipher" do
-    it "can encode" do
+    it "Can encode" do
       cipher = SimpleCipher.new
-      cipher.encode("aaaaaaaaaa").should eq(cipher.key[0, 10])
+      cipher.encode("aaaaaaaaaa").should eq(cipher.key[0, "aaaaaaaaaa".size])
     end
 
-    it "can decode" do
+    pending "Can decode" do
       cipher = SimpleCipher.new
-      cipher.decode(cipher.key[0, 10]).should eq("aaaaaaaaaa")
+      cipher.decode(cipher.key[0, "aaaaaaaaaa".size]).should eq("aaaaaaaaaa")
     end
 
-    it "is reversible" do
+    pending "Is reversible. I.e., if you apply decode in a encoded result, you must see the same plaintext encode parameter as a result of the decode method" do
       cipher = SimpleCipher.new
       cipher.decode(cipher.encode("abcdefghij")).should eq("abcdefghij")
     end
 
-    it "key is made only of lowercase letters" do
+    pending "Key is made only of lowercase letters" do
       cipher = SimpleCipher.new
       /^[a-z]+$/.matches?(cipher.key).should be_true
     end
   end
 
-  describe "Substituion cipher" do
-    it "can encode" do
+  describe "Substitution cipher" do
+    pending "Can encode" do
       cipher = SimpleCipher.new("abcdefghij")
       cipher.encode("aaaaaaaaaa").should eq("abcdefghij")
     end
 
-    it "can decode" do
+    pending "Can decode" do
       cipher = SimpleCipher.new("abcdefghij")
       cipher.decode("abcdefghij").should eq("aaaaaaaaaa")
     end
 
-    it "is reversible" do
+    pending "Is reversible. I.e., if you apply decode in a encoded result, you must see the same plaintext encode parameter as a result of the decode method" do
       cipher = SimpleCipher.new("abcdefghij")
       cipher.decode(cipher.encode("abcdefghij")).should eq("abcdefghij")
     end
 
-    it "can double shift encode" do
+    pending "Can double shift encode" do
       cipher = SimpleCipher.new("iamapandabear")
       cipher.encode("iamapandabear").should eq("qayaeaagaciai")
     end
 
-    it "can wrap on encode" do
+    pending "Can wrap on encode" do
       cipher = SimpleCipher.new("abcdefghij")
       cipher.encode("zzzzzzzzzz").should eq("zabcdefghi")
     end
 
-    it "can wrap on decode" do
+    pending "Can wrap on decode" do
       cipher = SimpleCipher.new("abcdefghij")
       cipher.decode("zabcdefghi").should eq("zzzzzzzzzz")
     end
 
-    it "can encode messages longer than the key" do
+    pending "Can encode messages longer than the key" do
       cipher = SimpleCipher.new("abc")
       cipher.encode("iamapandabear").should eq("iboaqcnecbfcr")
     end
 
-    it "can decode messages longer than the key" do
+    pending "Can decode messages longer than the key" do
       cipher = SimpleCipher.new("abc")
       cipher.decode("iboaqcnecbfcr").should eq("iamapandabear")
     end

2f8704c2afdbc493b47778a74386b4db179f8a5f
Ryan Hartlage <ryanplusplus@gmail.com>
2023-10-07 10:35:45 -0400

    Add example

--- exercises/practice/complex-numbers/spec/complex_numbers_spec.cr
+++ exercises/practice/complex-numbers/spec/complex_numbers_spec.cr
@@ -27,55 +27,55 @@ describe "ComplexNumbers" do
   end
 
   pending "Imaginary unit" do
-    (Complex.new(0, 1) * (Complex.new(0, 1))).should eq Complex.new(-1, 0)
+    (Complex.new(0, 1) * Complex.new(0, 1)).should eq Complex.new(-1, 0)
   end
 
   pending "Add purely real numbers" do
-    (Complex.new(1, 0) + (Complex.new(2, 0))).should eq Complex.new(3, 0)
+    (Complex.new(1, 0) + Complex.new(2, 0)).should eq Complex.new(3, 0)
   end
 
   pending "Add purely imaginary numbers" do
-    (Complex.new(0, 1) + (Complex.new(0, 2))).should eq Complex.new(0, 3)
+    (Complex.new(0, 1) + Complex.new(0, 2)).should eq Complex.new(0, 3)
   end
 
   pending "Add numbers with real and imaginary part" do
-    (Complex.new(1, 2) + (Complex.new(3, 4))).should eq Complex.new(4, 6)
+    (Complex.new(1, 2) + Complex.new(3, 4)).should eq Complex.new(4, 6)
   end
 
   pending "Subtract purely real numbers" do
-    (Complex.new(1, 0) - (Complex.new(2, 0))).should eq Complex.new(-1, 0)
+    (Complex.new(1, 0) - Complex.new(2, 0)).should eq Complex.new(-1, 0)
   end
 
   pending "Subtract purely imaginary numbers" do
-    (Complex.new(0, 1) - (Complex.new(0, 2))).should eq Complex.new(0, -1)
+    (Complex.new(0, 1) - Complex.new(0, 2)).should eq Complex.new(0, -1)
   end
 
   pending "Subtract numbers with real and imaginary part" do
-    (Complex.new(1, 2) - (Complex.new(3, 4))).should eq Complex.new(-2, -2)
+    (Complex.new(1, 2) - Complex.new(3, 4)).should eq Complex.new(-2, -2)
   end
 
   pending "Multiply purely real numbers" do
-    (Complex.new(1, 0) * (Complex.new(2, 0))).should eq Complex.new(2, 0)
+    (Complex.new(1, 0) * Complex.new(2, 0)).should eq Complex.new(2, 0)
   end
 
   pending "Multiply purely imaginary numbers" do
-    (Complex.new(0, 1) * (Complex.new(0, 2))).should eq Complex.new(-2, 0)
+    (Complex.new(0, 1) * Complex.new(0, 2)).should eq Complex.new(-2, 0)
   end
 
   pending "Multiply numbers with real and imaginary part" do
-    (Complex.new(1, 2) * (Complex.new(3, 4))).should eq Complex.new(-5, 10)
+    (Complex.new(1, 2) * Complex.new(3, 4)).should eq Complex.new(-5, 10)
   end
 
   pending "Divide purely real numbers" do
-    (Complex.new(1, 0) / (Complex.new(2, 0))).should eq Complex.new(0.5, 0)
+    (Complex.new(1, 0) / Complex.new(2, 0)).should eq Complex.new(0.5, 0)
   end
 
   pending "Divide purely imaginary numbers" do
-    (Complex.new(0, 1) / (Complex.new(0, 2))).should eq Complex.new(0.5, 0)
+    (Complex.new(0, 1) / Complex.new(0, 2)).should eq Complex.new(0.5, 0)
   end
 
   pending "Divide numbers with real and imaginary part" do
-    (Complex.new(1, 2) / (Complex.new(3, 4))).should eq Complex.new(0.44, 0.08)
+    (Complex.new(1, 2) / Complex.new(3, 4)).should eq Complex.new(0.44, 0.08)
   end
 
   pending "Absolute value of a positive purely real number" do
@@ -119,7 +119,7 @@ describe "ComplexNumbers" do
   end
 
   pending "Exponential of a purely real number" do
-    Complex.new(1, 0).exp.should eq Complex.new(e, 0)
+    Complex.new(1, 0).exp.should eq Complex.new(Math::E, 0)
   end
 
   pending "Exponential of a number with real and imaginary part" do
@@ -131,34 +131,34 @@ describe "ComplexNumbers" do
   end
 
   pending "Add real number to complex number" do
-    (Complex.new(1, 2) + (5)).should eq Complex.new(6, 2)
+    (Complex.new(1, 2) + 5).should eq Complex.new(6, 2)
   end
 
   pending "Add complex number to real number" do
-    (5 + (Complex.new(1, 2))).should eq Complex.new(6, 2)
+    (5 + Complex.new(1, 2)).should eq Complex.new(6, 2)
   end
 
   pending "Subtract real number from complex number" do
-    (Complex.new(5, 7) - (4)).should eq Complex.new(1, 7)
+    (Complex.new(5, 7) - 4).should eq Complex.new(1, 7)
   end
 
   pending "Subtract complex number from real number" do
-    (4 - (Complex.new(5, 7))).should eq Complex.new(-1, -7)
+    (4 - Complex.new(5, 7)).should eq Complex.new(-1, -7)
   end
 
   pending "Multiply complex number by real number" do
-    (Complex.new(2, 5) * (5)).should eq Complex.new(10, 25)
+    (Complex.new(2, 5) * 5).should eq Complex.new(10, 25)
   end
 
   pending "Multiply real number by complex number" do
-    (5 * (Complex.new(2, 5))).should eq Complex.new(10, 25)
+    (5 * Complex.new(2, 5)).should eq Complex.new(10, 25)
   end
 
   pending "Divide complex number by real number" do
-    (Complex.new(10, 100) / (10)).should eq Complex.new(1, 10)
+    (Complex.new(10, 100) / 10).should eq Complex.new(1, 10)
   end
 
   pending "Divide real number by complex number" do
-    (5 / (Complex.new(1, 1))).should eq Complex.new(2.5, -2.5)
+    (5 / Complex.new(1, 1)).should eq Complex.new(2.5, -2.5)
   end
 end

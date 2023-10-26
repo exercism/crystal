0f100515ab46145b46fcfe7b9c72a7cd8c3e5349
Pete Johns <paj+github@johnsy.com>
2023-10-26 19:57:04 +1100

    Improve output for `crystal spec -v`

--- exercises/practice/resistor-color/spec/resistor_color_spec.cr
+++ exercises/practice/resistor-color/spec/resistor_color_spec.cr
@@ -1,24 +1,28 @@
 require "spec"
 require "../src/*"
 
-describe "ResistorColor" do
-  it "Black" do
-    ResistorColor.color_code("black").should eq(0)
+describe ResistorColor do
+  describe ".color_code" do
+    it "finds the value for black" do
+      ResistorColor.color_code("black").should eq(0)
+    end
+
+    pending "finds the value for white" do
+      ResistorColor.color_code("white").should eq(9)
+    end
+
+    pending "finds the value for orange" do
+      ResistorColor.color_code("orange").should eq(3)
+    end
+
+    pending "handles input that is not a recognised color" do
+      ResistorColor.color_code("Puce").should be_nil
+    end
   end
 
-  pending "White" do
-    ResistorColor.color_code("white").should eq(9)
-  end
-
-  pending "Orange" do
-    ResistorColor.color_code("orange").should eq(3)
-  end
-
-  pending "handles input that is not a recognised color" do
-    ResistorColor.color_code("Puce").should be_nil
-  end
-
-  pending "Colors" do
-    ResistorColor.colors.should eq(["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"])
+  describe ".colors" do
+    pending "returns an array of known colors" do
+      ResistorColor.colors.should eq(["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"])
+    end
   end
 end

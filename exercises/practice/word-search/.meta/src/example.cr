b44fa9a55b860078426074913cbfd2a34d8a08ae
Ryan Hartlage <ryanplusplus@gmail.com>
2023-11-04 15:34:07 -0400

    Simplified example

--- exercises/practice/word-search/.meta/src/example.cr
+++ exercises/practice/word-search/.meta/src/example.cr
@@ -3,74 +3,37 @@ module WordSearch
 
   def self.search(grid, words)
     grid = grid.map(&.chars)
-    row_count = grid.size
-    column_count = grid[0].size
     results = Hash(String, Location | Nil).new
 
     words.each do |word|
       results[word] = nil
 
-      grid.each_with_index do |row, r|
-        if c = row.join.index(word)
-          results[word] = { {c + 1, r + 1}, {c + word.size, r + 1} }
-          break
-        end
-
-        if c = row.join.index(word.reverse)
-          results[word] = { {c + word.size, r + 1}, {c + 1, r + 1} }
-          break
-        end
-      end
-
-      grid.transpose.each_with_index do |row, r|
-        if c = row.join.index(word)
-          results[word] = { {r + 1, c + 1}, {r + 1, c + word.size} }
-          break
-        end
-
-        if c = row.join.index(word.reverse)
-          results[word] = { {r + 1, c + word.size}, {r + 1, c + 1} }
-          break
-        end
-      end
-
-      ((-column_count + 1)...column_count).each do |starting_column|
-        diagonal = (0...row_count).compact_map do |row|
-          grid[row][row + starting_column]? if row + starting_column >= 0
-        end
-
-        start = {[starting_column, 0].max, [-starting_column, 0].max}
-
-        if c = diagonal.join.index(word)
-          results[word] = { {start[0] + c + 1, start[1] + c + 1}, {start[0] + word.size, start[1] + word.size} }
-          break
-        end
-
-        if c = diagonal.join.index(word.reverse)
-          results[word] = { {start[0] + c + word.size, start[1] + c + word.size}, {start[0] + c + 1, start[1] + c + 1} }
-          break
-        end
-      end
-
-      (0...(2 * column_count - 1)).each do |starting_column|
-        diagonal = (0...row_count).compact_map do |row|
-          grid[row][starting_column - row]? if starting_column - row >= 0
-        end.reverse
-
-        start = {[starting_column - row_count + 1, 0].max, [starting_column, column_count - 1].min}
-
-        if c = diagonal.join.index(word)
-          results[word] = { {start[0] + c + 1, start[1] - c + 1}, {start[0] + c + word.size, start[1] - word.size} }
-          break
-        end
-
-        if c = diagonal.join.index(word.reverse)
-          results[word] = { {start[0] + c + word.size, start[1] - word.size + 1}, {start[0] + c + 1, start[1] - c + 1} }
-          break
+      (0...grid[0].size).each do |x|
+        (0...grid.size).each do |y|
+          each_delta do |dx, dy|
+            if end_point = find_end(grid, x, y, dx, dy, word)
+              results[word] = { {x + 1, y + 1}, end_point }
+            end
+          end
         end
       end
     end
 
     results
+  end
+
+  private def self.each_delta
+    [-1, 0, 1].each do |dx|
+      [-1, 0, 1].each do |dy|
+        yield(dx, dy) if dx != 0 || dy != 0
+      end
+    end
+  end
+
+  private def self.find_end(grid, x, y, dx, dy, word)
+    return nil unless (0...grid[0].size).includes?(x) && (0...grid.size).includes?(y)
+    return nil unless grid[y][x] == word[0]
+    return {x + 1, y + 1} if word.size == 1
+    find_end(grid, x + dx, y + dy, dx, dy, word[1...])
   end
 end

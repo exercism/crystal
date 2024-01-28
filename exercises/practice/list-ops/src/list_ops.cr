80af778a8155630ad4ef55fd282168ea27ebdc6f
Kah Goh <villastar@yahoo.com.au>
2024-01-28 22:46:38 +0800

    [New Exercise]: List Ops

---
+++ exercises/practice/list-ops/src/list_ops.cr
@@ -0,0 +1,27 @@
+class ListOps(T)
+  # Write your code for the 'Simple Cipher' exercise in this file.
+
+  def append(list1 : Array(T), list2 : Array(T)) : Array(T)
+  end
+
+  def concat(lists : Array(Array(T)))
+  end
+
+  def filter(list : Array(T), function : T -> Bool) : Array(T)
+  end
+
+  def length(list : Array(T)) : Int
+  end
+
+  def map(list : Array(T), function : T -> _)
+  end
+
+  def foldl(list : Array(T), initial : _, function : _, T -> _)
+  end
+
+  def foldr(list : Array(T), initial : _, function : _, T -> _)
+  end
+
+  def reverse(list : Array(T)) : Array(T)
+  end
+end

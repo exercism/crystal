e9ba4b67cd1b02c34858a659ab9d4381f6c2e735
meatball <meatballsofmeatballs@gmail.com>
2026-03-25 13:00:07 +0100

    Add nix support

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Dev environment with latest nixpkgs and Crystal";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux"; # change if needed
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          crystal
+        ];
+      };
+    };
+}

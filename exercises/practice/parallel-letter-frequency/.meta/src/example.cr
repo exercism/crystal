e9c54ac73844595fa58476efe56be165e3bebc22
meatball <meatballsofmeatballs@gmail.com>
2026-03-25 14:30:32 +0100

    Add Crystal 1.19 new concurrent datatypes

--- exercises/practice/parallel-letter-frequency/.meta/src/example.cr
+++ exercises/practice/parallel-letter-frequency/.meta/src/example.cr
@@ -1,29 +1,34 @@
+require "sync/shared"
+
 class ParallelLetterFrequency
   def self.calculate_frequencies(input : Array(String)) : Hash(String, Int32)
-    frequencies = Hash(String, Int32).new(0)
-    channel = Channel(Hash(String, Int32)).new
-
+    data : Sync::Shared(Hash(String, Int32)) = Sync::Shared.new({} of String => Int32)
+    channel = Channel(Nil).new
     input.each do |text|
       spawn do
         local = Hash(String, Int32).new(0)
-        processed = 0
 
         text.each_char do |char|
           next unless char.letter?
           local[char.downcase.to_s] += 1
-
-          # Crystal fibers are cooperatively scheduled; yielding occasionally
-          # prevents one long-running fiber from starving the others.
         end
-        channel.send(local)
+
+        data.replace do |shared_data|
+          local.each do |char, count|
+            shared_data[char] ||= 0
+            shared_data[char] += count
+          end
+          shared_data
+        end
+        channel.send(nil)  # signal completion
       end
     end
-    input.size.times do |x|
-      hash = channel.receive
-      frequencies.merge!(hash) do |key, oldval, newval|
-        oldval + newval
-      end
+
+    input.size.times do
+      channel.receive
     end
-    frequencies
+
+
+    data.get()
   end
 end

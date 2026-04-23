f7b8cbc9748c3a698bb12eed9c7eef65f97cec7e
Carl Harrison <meatballsofmeatballs@gmail.com>
2026-04-23 14:18:33 +0200

    Format grep file

--- exercises/practice/grep/src/grep.cr
+++ exercises/practice/grep/src/grep.cr
@@ -1,5 +1,5 @@
 module Grep
   def self.search(pattern : String, flags : Array(String), files : Array(String)) : String
-     # Write your code for the 'grep' exercise in this file. 
+    # Write your code for the 'grep' exercise in this file.
   end
 end
